2ec5c53bca7b76fe87e322f9dd441e735b594937
Lucas Levi <lucas.lsl12@gmail.com>
2021-10-27 21:27:10 -0300

    room model with lombok getter and setter

--- src/main/java/com/lucaslevi/meets/model/Room.java
+++ src/main/java/com/lucaslevi/meets/model/Room.java
@@ -1,4 +1,31 @@
 package com.lucaslevi.meets.model;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name="meetingroom")
 public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name="name", nullable = false)
+    private String name;
+
+    @Column(name="date", nullable = false)
+    private String date;
+
+    @Column(name="startHour", nullable = false)
+    private String startHour;
+
+    @Column(name="endHour", nullable = false)
+    private String endHour;
 }
